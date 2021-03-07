827bd4a9dbf850a120c55c64a3bd8ac8c84ed4f9
Karol Bochynski <kbochynski@opera.com>
2021-03-07 14:31:43 +0100

    CT: Add home fragment.

---
+++ app/src/main/java/com/kakaboc/chinesetrainer/HomeFragment.kt
@@ -0,0 +1,16 @@
+package com.kakaboc.chinesetrainer
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.kakaboc.chinesetrainer.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private lateinit var views: FragmentHomeBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        views = FragmentHomeBinding.bind(view)
+    }
+}
